4c4c898a0d108e48f253cae10a84268a13812598
sylvioneto <sylvio.pedroza@gmail.com>
2020-03-21 12:43:51 -0600

    move queryall to product

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	_ "github.com/lib/pq"
-	"github.com/sylvioneto/store-app/pkg/infra"
 	"github.com/sylvioneto/store-app/pkg/product"
 )
 
@@ -19,28 +18,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := queryAllProducts()
+	products := product.QueryAll()
 	webTemplates.ExecuteTemplate(w, "Index", products)
-}
-
-func queryAllProducts() []product.Product {
-	db := infra.ConnectToDatabase()
-	defer db.Close()
-
-	rows, err := db.Query("select id, name, price, quantity from items")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	var products []product.Product
-	for rows.Next() {
-		var product product.Product
-		err = rows.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		log.Println(product)
-		products = append(products, product)
-	}
-	return products
 }
--- pkg/product/product.go
+++ pkg/product/product.go
@@ -1,5 +1,10 @@
 package product
 
+import (
+	"log"
+	"github.com/sylvioneto/store-app/pkg/infra"
+)
+
 // Product is the structure that defines store item
 type Product struct {
 	ID int
@@ -14,6 +19,28 @@ func GetTestData() []Product {
 		{1, "Rice", 11.99, 5},
 		{2, "Water", 0.99, 20},
 		{3, "Meat", 29.11, 1},
+	}
+	return products
+}
+
+func QueryAll() []Product {
+	db := infra.ConnectToDatabase()
+	defer db.Close()
+
+	rows, err := db.Query("select id, name, price, quantity from items")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	var products []Product
+	for rows.Next() {
+		var product Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		log.Println(product)
+		products = append(products, product)
 	}
 	return products
 }
