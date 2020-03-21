0421ffcfed436a70d3412a6af89f0e744e257b36
sylvioneto <sylvio.pedroza@gmail.com>
2020-03-21 11:33:15 -0600

    add db connection

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
-	"github.com/sylvioneto/store-app/pkg"
+
+	_ "github.com/lib/pq"
+	"github.com/sylvioneto/store-app/pkg/items"
 )
 
 var webTemplates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	log.Println("Starting webserver...")
+	connectToDatabase()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
@@ -18,4 +22,14 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	items := items.GetTestData()
 	webTemplates.ExecuteTemplate(w, "index.html", items)
+}
+
+func connectToDatabase() {
+	connStr := "dbname=storedb host=localhost port=54320 sslmode=disable user=postgres password=mysecretpassword"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Ping()
+	log.Println("connect to db: success")
 }
--- scripts/dbstart.sh
+++ scripts/dbstart.sh
@@ -1,5 +1,5 @@
 docker run --name postgres-store \
 -e POSTGRES_PASSWORD=mysecretpassword \
 -e POSTGRES_DB=storedb \
--p 5432:54320 \
+-p 54320:5432 \
 -d postgres 
